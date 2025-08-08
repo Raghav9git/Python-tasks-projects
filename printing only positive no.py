2dfe146f75083603a321aff840785acc1f36f6d2
Raghav Sathe <raghavgsathe9@gmail.com>
2025-08-08 15:14:43 +0530

    Add files via upload

---
+++ printing only positive no.py
@@ -0,0 +1,9 @@
+List1 = [12,-7,5,64,-14]
+for number in List1:
+    if number > 0:
+        print(number)
+
+List2 = [12,14,-95,3]
+for number2 in List2:
+    if number2 > 0:
+        print(number2)
