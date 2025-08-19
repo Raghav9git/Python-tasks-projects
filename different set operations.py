7fc73531fa00a46a6fe6cb91611bf07a76d05957
Raghav Sathe <raghavgsathe9@gmail.com>
2025-08-19 16:48:10 +0530

    Update different set operations.py

--- different set operations.py
+++ different set operations.py
@@ -1,15 +1,16 @@
-# Define two sets
+# Defining two sets
 E = {0, 2, 4, 6, 8}
 N = {1, 2, 3, 4, 5}
 
-# Perform set operations
+# Performing set operations
 union_set = E | N
 intersection_set = E & N
 difference_set = E - N
 symmetric_difference_set = E ^ N
 
-# Display the results
+# Printing the results
 print("Union of E and N is", union_set)
 print("Intersection of E and N is", intersection_set)
 print("Difference of E and N is", difference_set)
 print("Symmetric difference of E and N is", symmetric_difference_set)
+
